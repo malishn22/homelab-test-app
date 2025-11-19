b26a3b650ff54bc44b05cc33c38d90a0a9c2d397
Mali <mali.sahin2222@gmail.com>
2025-11-19 21:13:22 +0300

    Update message

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+
 @app.get("/")
 def home():
-    return "Homelab is alive!"
+    app.logger.info("Home endpoint called")
+    return "Homelab Test with logging"
 
 app.run(host="0.0.0.0", port=5000)
 
